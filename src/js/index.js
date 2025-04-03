8a67aa1282a1ac4cf9830e3824f834e80213c138
Aleksandr <sanchezzkusika@gmail.com>
2025-04-04 02:52:35 +0300

    added logic for calculating the basket cost

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,7 @@
 import getByJSON from './modules/getByJSON';
 import getCartInfo from './modules/cart';
+import calcCartPriceAndDelivery from './modules/calcCartPrice';
 
 getByJSON();
+calcCartPriceAndDelivery();
 getCartInfo();
