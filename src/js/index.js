6aa338dd1237c0f67ecb50d590a8df66a8c68910
Aleksandr <sanchezzkusika@gmail.com>
2025-04-04 03:03:53 +0300

    added logic for switching cart status

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,9 @@
-import getByJSON from './modules/getByJSON';
-import getCartInfo from './modules/cart';
-import calcCartPriceAndDelivery from './modules/calcCartPrice';
+import getByJSON from "./modules/getByJSON";
+import calcCartPriceAndDelivery from "./modules/calcCartPrice";
+import toggleCartStatus from "./modules/toggleCartStatus";
+import getCartInfo from "./modules/cart";
 
 getByJSON();
 calcCartPriceAndDelivery();
+toggleCartStatus();
 getCartInfo();
