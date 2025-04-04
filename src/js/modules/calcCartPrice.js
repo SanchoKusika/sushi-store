662c6970a8585fa84304516f64277d554e5de26a
Aleksandr <sanchezzkusika@gmail.com>
2025-04-04 03:52:22 +0300

    now delivery is added to the receipt

--- src/js/modules/calcCartPrice.js
+++ src/js/modules/calcCartPrice.js
@@ -6,28 +6,28 @@ function calcCartPriceAndDelivery() {
 	const cartDelivery = document.querySelector("[data-cart-delivery]");
 
 	let priceTotal = 0;
-
-	priceElements.forEach(function(item) {
+	priceElements.forEach((item) => {
 		const amountEl = item
 			.closest(".cart-item")
 			.querySelector("[data-counter]");
 		priceTotal += parseInt(item.innerText) * parseInt(amountEl.innerText);
 	});
 
-	totalPriceEl.innerText = priceTotal;
+	let deliveryCostValue = priceTotal > 0 && priceTotal < 600 ? 250 : 0;
 
-	if (priceTotal > 0) {
-		cartDelivery.classList.remove("none");
-	} else {
-		cartDelivery.classList.add("none");
-	}
-	if (priceTotal >= 600) {
+	const totalWithDelivery = priceTotal + deliveryCostValue;
+
+	totalPriceEl.innerText = totalWithDelivery;
+
+	if (priceTotal >= 600 || priceTotal === 0) {
 		deliveryCost.classList.add("free");
 		deliveryCost.innerText = "бесплатно";
 	} else {
 		deliveryCost.classList.remove("free");
 		deliveryCost.innerText = "250 ₽";
 	}
+
+	cartDelivery.classList.toggle("none", priceTotal === 0);
 }
 
 export default calcCartPriceAndDelivery;
