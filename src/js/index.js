b9b47287e9fb8aa46b20a6533367927ac0d4087d
Aleksandr <sanchezzkusika@gmail.com>
2025-04-10 22:32:55 +0300

    generation of cards via json has been removed, now they are output directly

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,7 @@
-import getByJSON from "./modules/getByJSON";
 import countProduct from "./modules/counter";
 import getCartInfo from "./modules/cart";
 import adminTabs from "./modules/adminTabs";
 
-getByJSON();
 countProduct();
 getCartInfo();
 adminTabs();
