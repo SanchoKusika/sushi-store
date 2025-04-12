a86660bc5616303da4a04d17feddb9e6e90dc15d
Aleksandr <sanchezzkusika@gmail.com>
2025-04-12 19:14:50 +0300

    index.php code is more clear now

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ include(ROOT . "templates/header.tpl");
 <main>
 	<div class="container mb-5">
 		<div class="row">
-		<div class="col-md-8">
+			<div class="col-md-8">
 				<div class="row">
 					<?php foreach ($products as $product): ?>
 						<div class="col-md-6">
@@ -50,22 +50,14 @@ include(ROOT . "templates/header.tpl");
 				</div>
 			</div>
 
-			<!-- Корзина -->
 			<div class="col-md-4">
-				<!-- Корзина -->
 				<div class="card mb-4">
 					<div class="card-body">
 						<h5 class="card-title">Ваш заказ</h5>
-
 						<div data-cart-empty class="alert alert-secondary" role="alert">
 							Корзина пуста
 						</div>
-
-						<!-- cart-wrapper -->
 						<div class="cart-wrapper"></div>
-						<!-- // cart-wrapper -->
-
-						<!-- Стоимость заказа -->
 						<div class="cart-total">
 							<p data-cart-delivery class="none">
 								<span class="h5">Доставка:</span>
@@ -78,35 +70,20 @@ include(ROOT . "templates/header.tpl");
 								<span class="rouble">₽</span>
 							</p>
 						</div>
-						<!-- // Стоимость заказа -->
 					</div>
-
-					<!-- Оформить заказ -->
 					<div id="order-form" class="card-body border-top none">
 						<h5 class="card-title">Оформить заказ</h5>
 						<form>
 							<div class="form-group">
 								<input type="text" class="form-control" placeholder="Ваше имя" required name="name" minlength="2" pattern="[А-Яа-яA-Za-z\s]+" />
-
 								<input type="tel" class="form-control mt-2" placeholder="Ваш номер телефона" required name="phone" />
-
-								<input
-									type="email"
-									class="form-control mt-2"
-									placeholder="Ваш email"
-									required
-									name="email"
-									pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"
-								/>
-
+								<input type="email" class="form-control mt-2" placeholder="Ваш email" required name="email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" />
 								<input type="text" class="form-control mt-2" placeholder="Адрес доставки" required name="address" minlength="5" />
 							</div>
-							<button type="submit" class="btn btn-block btn-outline-warning">Заказать</button>
+							<button type="submit" class="btn btn-block btn-outline-warning mt-3">Заказать</button>
 						</form>
 					</div>
-					<!-- // Оформить заказ -->
 				</div>
-				<!-- // Корзина -->
 			</div>
 		</div>
 	</div>
